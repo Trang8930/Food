38c47b47e1aba7a79a9256d39b50d9c6bdb3a26c
Nguyen Phu <nvphu1306@gmail.com>
2019-03-04 17:43:16 +0700

    optimize code

--- src/components/Blog/Blog-single/BlogSingle.jsx
+++ src/components/Blog/Blog-single/BlogSingle.jsx
@@ -1,40 +1,15 @@
 import React, { Component } from 'react';
 
 class BlogSingle extends Component {
+    
     render() {
         return (
-            <React.Fragment>
-  <nav className="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-    <div className="container">
-      <a className="navbar-brand" href="index.html">Coffee<small>Blend</small></a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="oi oi-menu" /> Menu
-      </button>
-      <div className="collapse navbar-collapse" id="ftco-nav">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item"><a href="index.html" className="nav-link">Home</a></li>
-          <li className="nav-item"><a href="menu.html" className="nav-link">Menu</a></li>
-          <li className="nav-item"><a href="services.html" className="nav-link">Services</a></li>
-          <li className="nav-item active"><a href="blog.html" className="nav-link">Blog</a></li>
-          <li className="nav-item"><a href="about.html" className="nav-link">About</a></li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="room.html" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Shop</a>
-            <div className="dropdown-menu" aria-labelledby="dropdown04">
-              <a className="dropdown-item" href="shop.html">Shop</a>
-              <a className="dropdown-item" href="product-single.html">Single Product</a>
-              <a className="dropdown-item" href="cart.html">Cart</a>
-              <a className="dropdown-item" href="checkout.html">Checkout</a>
-            </div>
-          </li>
-          <li className="nav-item"><a href="contact.html" className="nav-link">Contact</a></li>
-          <li className="nav-item cart"><a href="cart.html" className="nav-link"><span className="icon icon-shopping_cart" /><span className="bag d-flex justify-content-center align-items-center"><small>1</small></span></a></li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+        <React.Fragment>
+    <Bar/>
   {/* END nav */}
+  <div className="slider-item" style={{ height: 100, width: 100, backgroundImage: `url("images/image_1.jpg")`}} data-stellar-background-ratio="0.5"/>
   <section className="home-slider owl-carousel">
-    <div className="slider-item" style={{backgroundImage: 'url(images/bg_3.jpg)'}} data-stellar-background-ratio="0.5">
+    <div className="slider-item" style={{backgroundImage: `url("images/image_1.jpg")`}} data-stellar-background-ratio="0.5">
       <div className="overlay" />
       <div className="container">
         <div className="row slider-text justify-content-center align-items-center">
--- src/index.js
+++ src/index.js
@@ -1,35 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import Home from './components/Home/Home';
-import Menu from './components/Menu/Menu';
-import Service from './components/Service/Service';
-import Blog from './components/Blog/Blog';
-import BlogSingle from './components/Blog/Blog-single/BlogSingle';
-import About from './components/About/About';
-import Shop from './components/Shop/Shop';
-import ProductSingle from './components/Shop/ProductSingle/ProductSingle';
-import Cart from './components/Shop/Cart/Cart';
-import Checkout from './components/Shop/Checkout/Checkout';
-import Contact from './components/Contact/Contact';
-
-
-ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route path = '/' exact component = { Home } />
-            <Route path = '/Menu' exact component = { Menu } />
-            <Route path = '/Service' exact component = { Service } />
-            <Route path = '/Blog' exact component = { Blog } />
-            <Route path = '/BlogSingle' exact component = { BlogSingle } />
-            <Route path = '/About' exact component = { About } />
-            <Route path = '/Shop' exact component = { Shop } />
-            <Route path = '/ProductSingle' exact component = { ProductSingle } />
-            <Route path = '/Cart' exact component = { Cart } />
-            <Route path = '/Checkout' exact component = { Checkout } />
-            <Route path = '/Contact' exact component = { Contact } />
-        </Switch>
-    </Router>
-    , document.getElementById('root'));
+import Com from './index_component.js'
+ReactDOM.render(<Com/>, document.getElementById('root'));
 serviceWorker.unregister();
